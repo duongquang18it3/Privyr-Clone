7ee5f6680a2094ced043789e2b83ac992c351e42
Tran Duong Quang <tdquang.18it3@sict.udn.vn>
2022-07-07 12:21:04 +0700

    ok

--- src/components/floatButton/floatButton.tsx
+++ src/components/floatButton/floatButton.tsx
@@ -5,15 +5,15 @@ import { FaPlus } from "react-icons/fa";
 import * as styles from "./floatButton.module.scss";
 
 export const FloatButton = (): JSX.Element => {
-	return (
-		<>
-			<div className={classNames("mx-5 my-3", styles.floatButton)}>
-				<button className="button is-primary">
-					<span className="icon">
-						<FaPlus />
-					</span>
-				</button>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className={classNames("mx-5 my-3", styles.floatButton)}>
+        <button className="button is-info">
+          <span className="icon">
+            <FaPlus />
+          </span>
+        </button>
+      </div>
+    </>
+  );
 };
